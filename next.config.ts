1f906acb85f5d0818379f09292fb3b7cd6c15c7c
澤田　秀哉 <sawada@sawadaMac-Book-Pro.local>
2026-07-02 16:01:39 +0900

    Allow local network dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1", "192.168.2.80"]
+};
 
 export default nextConfig;
